7724043889c6d424799aa4963c06d1902897f751
gokulm <gokulnathmurugesan@gmail.com>
2021-11-21 18:20:47 -0500

    fixed header

--- src/App.tsx
+++ src/App.tsx
@@ -34,46 +34,49 @@ const App = (props: any) => {
 
   return (
     <>
-      <nav className="navbar navbar-default navbar-fixed-top">
+      <Router>
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+            </div>
+            {/* <div className="navbar-collapse collapse"> */}
+            <div className="navbar-collapse">
+              <ul className="nav navbar-nav">
+                <li>
+                  <Link to="/home">Home</Link>
+                </li>
+                <li>
+                  <Link to="/chapters">Chapters</Link>
+                </li>
+                <li><a ui-sref="sections"><span className="brand" ></span></a></li>
+                <li><a ui-sref="chaptergroups"><span className="brand" ></span></a></li>
+                <li><a ui-sref="search"><span className="brand"></span></a></li>
+              </ul>
+              <ul className="nav navbar-nav navbar-right">
+                <li className="dropdown">
+                  <a className="dropdown-toggle" data-toggle="dropdown" href="#" id="download" aria-expanded="false"><span className="brand"></span> <span className="caret"></span></a>
+                  <ul className="dropdown-menu" aria-labelledby="download">
+                    <li ng-repeat="lang in layout.AllowedLanguages" >
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="page-header"></div>
+
         <div className="container">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a ui-sref="thirukkuralsbychapters( { index: 1 })" className="navbar-brand"><span className="brand"></span></a>
-          </div>
-          <div className="navbar-collapse collapse" id="navbar">
-            <ul className="nav navbar-nav">
-              <li data-toggle="collapse" data-target=".navbar-collapse.in" ng-className="layout.IsActiveMenu(['thirukkuralsbychapters'])"><a ui-sref="thirukkuralsbychapters( { index: 1 })"><span className="brand">Test</span></a></li>
-              <li data-toggle="collapse" data-target=".navbar-collapse.in" ng-className="layout.IsActiveMenu(['chapters', 'sectionchapters', 'chaptergroupchapters'])"><a ui-sref="chapters"><span className="brand">Test 2</span></a></li>
-              <li data-toggle="collapse" data-target=".navbar-collapse.in" ng-className="layout.IsActiveMenu(['sections'])"><a ui-sref="sections"><span className="brand" ></span></a></li>
-              <li data-toggle="collapse" data-target=".navbar-collapse.in" ng-className="layout.IsActiveMenu(['chaptergroups'])"><a ui-sref="chaptergroups"><span className="brand" ></span></a></li>
-              <li data-toggle="collapse" data-target=".navbar-collapse.in" ng-className="layout.IsActiveMenu(['search'])"><a ui-sref="search"><span className="brand"></span></a></li>
-            </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li className="dropdown">
-                <a className="dropdown-toggle" data-toggle="dropdown" href="#" id="download" aria-expanded="false"><span className="brand"></span> <span className="caret"></span></a>
-                <ul className="dropdown-menu" aria-labelledby="download">
-                  <li ng-repeat="lang in layout.AllowedLanguages" data-toggle="collapse" data-target=".navbar-collapse.in" >
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <div className="page-header"></div>
-
-      <div className="container">
-
-        <div className="row">
-          <div className="box">
-            <div className="col-lg-12">
-              <div>
-                <Router>
+          <div className="row">
+            <div className="box">
+              <div className="col-lg-12">
+                <div>
                   <div>
                     <ul>
                       <li>
@@ -93,25 +96,25 @@ const App = (props: any) => {
                       <RouteWithSubRoutes key={i} {...route} />
                     ))}
                   </Switch>
-                </Router>
+                </div>
               </div>
             </div>
           </div>
+
         </div>
 
-      </div>
-
-      <footer>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12 text-center footer">
-              <a href="https://www.gokulnath.com/#contact" target="_blank">Contact</a> ::
-              <a href="http://blogs.gokulnath.com/2010/05/15/thirukkural" target="_blank">About</a>
+        <footer>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12 text-center footer">
+                <a href="https://www.gokulnath.com/#contact" target="_blank">Contact</a> ::
+                <a href="http://blogs.gokulnath.com/2010/05/15/thirukkural" target="_blank">About</a>
+              </div>
             </div>
           </div>
-        </div>
-      </footer>
+        </footer>
 
+      </Router>
     </>
   );
 }
