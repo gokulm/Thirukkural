2c9f4ba95b16064417e358a3c1efe3f82a272404
gokulm <gokulnathmurugesan@gmail.com>
2021-11-21 18:02:31 -0500

    added scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Home from './home/home'
 import Chapters from './chapters'
-import './App.css';
+import './App.scss';
 import {
   BrowserRouter as Router,
   Switch,
@@ -33,19 +33,46 @@ const App = (props: any) => {
   ];
 
   return (
-    <div>
-      <div className="container">
-        <div className="navbar-collapse collapse" id="navbar">
+    <>
+      <nav className="navbar navbar-default navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a ui-sref="thirukkuralsbychapters( { index: 1 })" className="navbar-brand"><span className="brand"></span></a>
+          </div>
+          <div className="navbar-collapse collapse" id="navbar">
+            <ul className="nav navbar-nav">
+              <li data-toggle="collapse" data-target=".navbar-collapse.in" ng-className="layout.IsActiveMenu(['thirukkuralsbychapters'])"><a ui-sref="thirukkuralsbychapters( { index: 1 })"><span className="brand">Test</span></a></li>
+              <li data-toggle="collapse" data-target=".navbar-collapse.in" ng-className="layout.IsActiveMenu(['chapters', 'sectionchapters', 'chaptergroupchapters'])"><a ui-sref="chapters"><span className="brand">Test 2</span></a></li>
+              <li data-toggle="collapse" data-target=".navbar-collapse.in" ng-className="layout.IsActiveMenu(['sections'])"><a ui-sref="sections"><span className="brand" ></span></a></li>
+              <li data-toggle="collapse" data-target=".navbar-collapse.in" ng-className="layout.IsActiveMenu(['chaptergroups'])"><a ui-sref="chaptergroups"><span className="brand" ></span></a></li>
+              <li data-toggle="collapse" data-target=".navbar-collapse.in" ng-className="layout.IsActiveMenu(['search'])"><a ui-sref="search"><span className="brand"></span></a></li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <li className="dropdown">
+                <a className="dropdown-toggle" data-toggle="dropdown" href="#" id="download" aria-expanded="false"><span className="brand"></span> <span className="caret"></span></a>
+                <ul className="dropdown-menu" aria-labelledby="download">
+                  <li ng-repeat="lang in layout.AllowedLanguages" data-toggle="collapse" data-target=".navbar-collapse.in" >
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className="page-header">header</div>
+      </nav>
+      <div className="page-header"></div>
 
       <div className="container">
 
         <div className="row">
           <div className="box">
             <div className="col-lg-12">
-              <div ui-view>
+              <div>
                 <Router>
                   <div>
                     <ul>
@@ -66,17 +93,6 @@ const App = (props: any) => {
                       <RouteWithSubRoutes key={i} {...route} />
                     ))}
                   </Switch>
-
-                  {/* <Switch>
-                    <Route path="/home">
-                      <Home />
-                    </Route>
-                    <Route exact path="/chapters">
-                      <Chapters />
-                    </Route>
-                    <Route path="/chapters/:chapterIndex/kurals" component={Home} />
-                    <Route path="/search" component={Search} />
-                  </Switch> */}
                 </Router>
               </div>
             </div>
@@ -89,13 +105,14 @@ const App = (props: any) => {
         <div className="container">
           <div className="row">
             <div className="col-lg-12 text-center footer">
-              <a href="http://gokulnath.com/#contact" target="_blank">Contact</a> ::
-              <a href="http://blogs.gokulnath.com//2010/05/15/Thirukural.aspx" target="_blank">About</a>
+              <a href="https://www.gokulnath.com/#contact" target="_blank">Contact</a> ::
+              <a href="http://blogs.gokulnath.com/2010/05/15/thirukkural" target="_blank">About</a>
             </div>
           </div>
         </div>
       </footer>
-    </div>
+
+    </>
   );
 }
 
