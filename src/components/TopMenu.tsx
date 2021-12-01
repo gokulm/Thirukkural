aa47d84d3b93ed40f84e8df7bd016d7108b837eb
gokulm <gokulnathmurugesan@gmail.com>
2021-12-01 08:04:22 -0500

    added active menu

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -6,34 +6,39 @@ import i18n from '../common/i18n';
 const TopMenu = (props: any) => {
     const { t } = useTranslation();
     const [language, setLanguage] = useState('tamil');
-    const { location } = props; 
+    const { location } = props;
 
     const switchLanguage = () => {
         if (language === 'tamil') {
-          setLanguage('english');
-          i18n.changeLanguage('english');
+            setLanguage('english');
+            i18n.changeLanguage('english');
         }
         else {
-          setLanguage('tamil');
-          i18n.changeLanguage('tamil');
+            setLanguage('tamil');
+            i18n.changeLanguage('tamil');
         }
-      }
-    
+    }
+
+    const isActiveMenu = (path: string, isExact: boolean = false): boolean => {
+        return isExact ? path === location.pathname :
+            (location.pathname as string).startsWith(path);
+    }
+
     return (
         <Navbar collapseOnSelect expand="lg" className="navbar-default ">
-          <Container>
-            <Navbar.Brand href="/">{t('WebsiteHeader')}</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="me-auto" activeKey={location.pathname}>
-                <Nav.Link href="/">{t('Kurals')}</Nav.Link>
-                <Nav.Link href="/chapters">{t('Chapters')}</Nav.Link>
-              </Nav>
-              <Nav>
-                <span className="switchLanguage" onClick={() => switchLanguage()}>{t('Language')}</span>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
+            <Container>
+                <Navbar.Brand href="/">{t('WebsiteHeader')}</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link active={isActiveMenu("/", true) || isActiveMenu("/chapters/")} href="/">{t('Kurals')}</Nav.Link>
+                        <Nav.Link active={isActiveMenu("/chapters", true)} href="/chapters">{t('Chapters')}</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <span className="switchLanguage" onClick={() => switchLanguage()}>{t('Language')}</span>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     );
 }
